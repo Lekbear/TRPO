1187679bd600de10e21fc145cbe5bbe0930c3043
kooi <kisson6@gmail.com>
2020-05-23 20:20:04 +0800

    Авторизация и регистация, работают, сделал дизайн красивый

--- NEWSITE/login/urls.py
+++ NEWSITE/login/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
-from django.views.generic import ListView,DetailView 
+from django.views.generic import ListView,DetailView
 from login.models import deadlines
 
 urlpatterns = [
-    path('', views.home, name = 'home'), #создать home страничку
+    #path('', views.home, name = 'home'), #создать home страничку
+    #path('home/', views.home, name ='home'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('register/', views.register, name='register'),
-    path('timetable/', views.timetable, name="timetable"),
-    path('deadlines/', 
-    ListView.as_view(queryset = deadlines.objects.all().order_by('deadlines'),
+    path('', views.timetable, name="timetable"),
+    path('deadlines/',ListView.as_view(queryset = deadlines.objects.all().order_by('deadlines'),
     template_name="/login/deadlines.html"), name = "deadlines"),
     path('deadlines/add', views.adddeadlines, name = 'adddeadlines'),
 ]
--- NEWSITE/login/views.py
+++ NEWSITE/login/views.py
@@ -4,32 +4,64 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 from .models import *
 from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as djanjologin
+from django.contrib.auth.decorators import login_required
 
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password = password)
+            if user is not None:
+                djanjologin(request, user)
+                return redirect('timetable')
+            else:
+                messages.info(request, 'Логин или пароль введен неверно')
+        context = {}
+        return render(request, 'login/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('timetable')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, user +' вы создали аккаунт.' )
+                return redirect('login')
+        context = {'form':form}
+        return render(request, 'login/register.html', context)
+
+@login_required(login_url='login')
 def home(request):
     return render(request, 'login/home.html')
 
-def login(request):
-    return render(request, 'login/login.html')
 
-def register(request):
-    #form = UserCreationForm()
-    form = CreateUserForm() # Форма с дополнениями(email) заменить
-    # Она лежит в forms.py
-    if request.method == 'POST':
-        #form = UserCreationForm(request.POST)
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-    context = {'form':form}
-    return render(request, 'login/register.html', context)
-
+@login_required(login_url='login')
 def timetable(request):
     courses = Course.objects.all()
     return render(request, 'login/timetable.html', {'courses':courses})
 
+@login_required(login_url='login')
 def adddeadlines(request):
     form2 = CreatedeadlinesForm(request.POST or None)
     if request.method == 'POST' and form2.is_valid():
-        form2.save() 
+        form2.save()
         return redirect('deadlines')
     return render(request, 'login/adddeadlines.html',{'form2':form2})
